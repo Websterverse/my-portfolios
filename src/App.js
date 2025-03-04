840df6a5d1c75b6d8309e25db5a75707d61d0af7
KARTIK KUMAR <118910009+Websterverse@users.noreply.github.com>
2025-03-04 12:18:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import './Home.css';
-import NAV from './Components/NAV'
-import HOME from './Components/HOME'
 
-
-
-import {BrowserRouter as Router , Routes , Route } from 'react-router-dom'
-import More from "../src/Components/More"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NAV from './Components/NAV';
+import HOME from './Components/HOME';
+import More from './Components/More';
 
 function App() {
   return (
-<Router>
-  <NAV />
-  <Routes>
-    <Route path="/" element={<HOME />} />
-    <Route path="/more" element={<More />} />
-  </Routes>
-</Router>
+    <Router>
+      <NAV />
+      <Routes>
+        <Route path="/" element={<HOME />} />
+        <Route path="/more" element={<More />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
